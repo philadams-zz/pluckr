cf45f32791824314bfe519ad9079826048bfed2f
Phil Adams <phil@philadams.net>
2013-02-21 08:16:25 -0500

    implement header row(s) skipping

--- pluckr/core.py
+++ pluckr/core.py
@@ -13,13 +13,6 @@ See README.txt for details.
 import csv
 
 
-def do_plucking(filenames):
-    pass
-
-def pluckr():
-    pass
-
-
 def cli():
     import argparse
     import sys
@@ -31,7 +24,7 @@ def cli():
     parser.add_argument('infile', nargs='?', default=sys.stdin,
             type=argparse.FileType('rU'), help='input file (.csv)')
     parser.add_argument('-f', '--fields', dest='fields',
-            help='the columns to grab')
+            help='the columns to grab (first column is 1)')
     parser.add_argument('-i', '--inverse', dest='inverse',
             help='invert the column selection: drop them instead')
     parser.add_argument('-d', '--delimiter', default=',', dest='delimiter',
@@ -39,11 +32,18 @@ def cli():
     parser.add_argument('-q', '--quotechar', default='"', dest='quotechar',
             help='field quotechar when reading infile')
     parser.add_argument('-s', '--skip', default=0, dest='skip',
-            help='number of rows to skip')
+            type=int, help='number of rows to skip')
     args = parser.parse_args()
 
+    # parse csv data
     rows = csv.reader(args.infile,
             delimiter=args.delimiter, quotechar=args.quotechar)
+
+    # skip n rows
+    for i in range(args.skip):
+        rows.next()
+
+
 
     out = csv.writer(sys.stdout)
     out.writerows(rows)
