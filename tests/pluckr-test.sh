8c8a1d2b2c514492cf7420b5230797213bf28ac7
Phil Adams <philadams.net@gmail.com>
2014-12-15 19:45:56 -0500

    clean up tests

--- tests/pluckr-test.sh
+++ tests/pluckr-test.sh
@@ -18,17 +18,19 @@ after() {
 }
 
 it_shows_help_on_h() {
-  $pluckr -h | head -n1 | grep 'usage'
+  #$pluckr -h | head -n1 | grep 'usage'
+  usage=$($pluckr -h | head -n1)
+  test "$usage" = "usage: pluckr [-h] [-f FIELDS] [-i] [-d DELIMITER] [-q QUOTECHAR] [-s SKIP]"
 }
 
-it_reads_from_stdin() {
+it_reads_from_piped_stdin() {
   out=$(cat $fixture | $pluckr | head -n1)
-  test $out=''
+  test "$out" = "first,last,preferred"
 }
 
 it_reads_from_file() {
   out=$($pluckr $fixture | head -n1)
-  test $out='first,last,preferred'
+  test "$out" = "first,last,preferred"
 }
 
 it_outputs_identity_with_no_args() {
@@ -37,13 +39,13 @@ it_outputs_identity_with_no_args() {
 }
 
 it_respects_col_order_requests() {
-  out=$($pluckr -f2,0,1 $fixture | head -n1)
-  test $out='preferred,first,last'
+  out=$($pluckr -f3,1,2 $fixture | head -n1)
+  test "$out" = "preferred,first,last"
 }
 
 it_understands_negative_indices() {
-  out=$($pluckr -f-1,0 $fixture | head -n1)
-  test $out='preferred,first'
+  out=$($pluckr -f-1,1 $fixture | head -n1)
+  test "$out" = "preferred,first"
 }
 
 it_shows_column_names() {
@@ -52,22 +54,22 @@ it_shows_column_names() {
   diff $tmp $tmp2
 }
 
-it_ignores_nonexistant_fields() {
-  out=$($pluckr $fixture -f2,17 | head -n1)
-  test $out='preferred'
+it_ignores_nonexistent_fields() {
+  out=$($pluckr $fixture -f3,17 | head -n1)
+  test "$out" = "preferred"
 }
 
 it_skips_header_rows() {
   out=$($pluckr -s1 $fixture | head -n1)
-  test $out='Malcolm,Reynolds,Mal'
+  test "$out" = "Malcolm,Reynolds,Mal"
 }
 
 it_handles_column_selection_inversion() {
-  out=$($pluckr $fixture -f1 -i | head -n1)
-  test $out='first,preferred'
+  out=$($pluckr $fixture -f2 -i | head -n1)
+  test "$out" = "first,preferred"
 }
 
 it_parameterizes_delimiters() {
   out=$($pluckr $fixturetsv -f-1,1 -d $'\t' | head -n1)
-  test $out='preferred,last'
+  test "$out" = "preferred,first"
 }
